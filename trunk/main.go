95b3a694d4b40cbc6a6ddbaf91040b048001d4df
ZackarySantana <zackzackyack@gmail.com>
2024-11-26 00:16:39 -0500

    fix: remove mongo log

--- trunk/main.go
+++ trunk/main.go
@@ -14,8 +14,6 @@ func main() {
 		port = ":" + p
 	}
 
-	fmt.Println("MONGO", os.Getenv("MONGODB_URL"))
-
 	fmt.Printf("Running on %s\n", port)
 	if err := http.ListenAndServe(port, api.New()); err != nil {
 		panic(err)
