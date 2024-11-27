b4be3ab5feeb2479ac2d40e7afe7a079309d9d8e
ZackarySantana <zackzackyack@gmail.com>
2024-11-26 20:04:11 -0500

    feat: setup leaves package with solid start template

---
+++ leaves/src/app.tsx
@@ -0,0 +1,29 @@
+import { createSignal } from "solid-js";
+import "./app.css";
+
+export default function App() {
+    const [count, setCount] = createSignal(0);
+
+    return (
+        <main>
+            <h1 class="text-4xl text-red-500">Hello world!</h1>
+            <button
+                class="increment"
+                onClick={() => setCount(count() + 1)}
+                type="button"
+            >
+                Clicks: {count()}
+            </button>
+            <p>
+                Visit{" "}
+                <a
+                    href="https://start.solidjs.com"
+                    target="_blank"
+                >
+                    start.solidjs.com
+                </a>{" "}
+                to learn how to build SolidStart apps.
+            </p>
+        </main>
+    );
+}
